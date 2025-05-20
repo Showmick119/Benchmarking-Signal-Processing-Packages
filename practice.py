31545ffb76582866772a45e759c03b5387ff7f6c
Showmick Das <showmickdas75@gmail.com>
2025-05-19 22:29:42 -0400

    Read csv data

--- practice.py
+++ practice.py
@@ -2,3 +2,5 @@ import pandas as pd
 import numpy as np
 from scipy.signal import butter, cheby1, cheby2, ellip, firwin, lfilter, medfilt
 from scipy.signal.windows import hamming, hann
+
+data = pd.read_csv("Practice-HRV/bangle.csv")
