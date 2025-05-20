49fefb40940ebc7353a433e490491b59fac4271f
Showmick Das <showmickdas75@gmail.com>
2025-05-20 08:48:42 -0400

    Loading datasets

--- practice.py
+++ practice.py
@@ -3,11 +3,19 @@ import numpy as np
 from scipy.signal import butter, cheby1, cheby2, ellip, firwin, lfilter, medfilt
 from scipy.signal.windows import hamming, hann
 
-'''Tasks:
+'''Outlined Tasks:
 - Plot the ECG and PPG data
 - Calculate the Standard Deviation of the RRIs (the time interval in milliseconds, between successsive R-wave peaks) (in the polarHRM.csv)
 - Try some of the processing techniques, like IIR and FIR filters, bandpass filtering, etc
 - Find peaks within the signal
 '''
 
-data = pd.read_csv("Practice-HRV/bangle.csv")
+"Loading All Datasets:"
+bangle_data = pd.read_csv("Practice-HRV/bangle.csv")
+print(bangle_data.head())
+
+polar_data = pd.read_csv("Practice-HRV/polar.csv")
+print(polar_data.head())
+
+polarHRM_data = pd.read_csv("Practice-HRV/polarHRM.csv")
+print(polarHRM_data.head())
