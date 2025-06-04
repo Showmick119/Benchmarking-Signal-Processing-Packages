7ae03ad040d870d92ac1cf216261e7fc165098b2
Showmick Das <showmickdas75@gmail.com>
2025-06-04 16:59:11 -0400

    Create XGBoost

--- models/model.py
+++ models/model.py
@@ -2,3 +2,5 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split()
+
+model = GradientBoostingRegressor()
